75594eaba14db1073f23c6fd45db0f7250c33d06
浅梦凉城 <m17762618644@163.com>
2021-06-22 14:39:08 +0800

    完成修改API

--- web/src/api/global.js
+++ web/src/api/global.js
@@ -35,26 +35,11 @@ export function getAuthor(success) {
  * 
  */
 export function getWebInfo(success) {
-    success({
-        success: true,
-        code: 200,
-        message: 'success',
-        data: {
-            item: {
-                articleCount: 11,
-                tagCount: 22,
-                classifyCount: 3,
-                notices: [
-                    '<a target="_blank" href="https://www.baidu.com">百度</a>',
-                    '网站测试通知，预祝网站成功完成',
-                ],
-                runTime: '1',
-                personCount: 2,
-                AllCount: 15,
-                accessCount: 1,
-                lastUpdate: '2021-02-22'
-            }
-        }
+    axios({
+        url: '/global/web_info',
+        method: 'GET'
+    }).then(res => {
+        success(res)
     })
 }
 
