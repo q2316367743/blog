7658d340c90ebe772065747df138c256c40be93b
浅梦凉城 <m17762618644@164.com>
2021-07-03 19:14:03 +0800

    修改API地址

--- web/src/utils/axios.js
+++ web/src/utils/axios.js
@@ -3,7 +3,7 @@ import { Message } from 'element-ui'
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8990/api',
+    baseURL: process.env.NODE_ENV === "development" ? 'http://localhost:8990/api' : './api',
     timeout: 3000
 })
 
