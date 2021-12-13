db8d4dce04cfcf1d5cf7870c1208453c9fd946e6
浅梦凉城 <m17762618644@163.com>
2021-12-13 09:56:04 +0800

    修改环境变量

--- manage/vue.config.js
+++ manage/vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
      * Detail: https://cli.vuejs.org/config/#publicpath
      */
     publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
-    outputDir: '../src/main/resources/static/manage',
+    outputDir: './dist',
     assetsDir: 'static',
     lintOnSave: process.env.NODE_ENV === 'development',
     productionSourceMap: false,
@@ -49,15 +49,13 @@ module.exports = {
     },
     chainWebpack(config) {
         // it can improve the speed of the first screen, it is recommended to turn on preload
-        config.plugin('preload').tap(() => [
-            {
-                rel: 'preload',
-                // to ignore runtime.js
-                // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-                fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-                include: 'initial'
-            }
-        ])
+        config.plugin('preload').tap(() => [{
+            rel: 'preload',
+            // to ignore runtime.js
+            // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+            fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+            include: 'initial'
+        }])
 
         // when there are many pages, it will cause too many meaningless requests
         config.plugins.delete('prefetch')
@@ -92,29 +90,29 @@ module.exports = {
                         .end()
                     config
                         .optimization.splitChunks({
-                        chunks: 'all',
-                        cacheGroups: {
-                            libs: {
-                                name: 'chunk-libs',
-                                test: /[\\/]node_modules[\\/]/,
-                                priority: 10,
-                                chunks: 'initial' // only package third parties that are initially dependent
-                            },
-                            elementUI: {
-                                name: 'chunk-elementUI', // split elementUI into a single package
-                                priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                            },
-                            commons: {
-                                name: 'chunk-commons',
-                                test: resolve('src/components'), // can customize your rules
-                                minChunks: 3, //  minimum common number
-                                priority: 5,
-                                reuseExistingChunk: true
+                            chunks: 'all',
+                            cacheGroups: {
+                                libs: {
+                                    name: 'chunk-libs',
+                                    test: /[\\/]node_modules[\\/]/,
+                                    priority: 10,
+                                    chunks: 'initial' // only package third parties that are initially dependent
+                                },
+                                elementUI: {
+                                    name: 'chunk-elementUI', // split elementUI into a single package
+                                    priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                                    test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                                },
+                                commons: {
+                                    name: 'chunk-commons',
+                                    test: resolve('src/components'), // can customize your rules
+                                    minChunks: 3, //  minimum common number
+                                    priority: 5,
+                                    reuseExistingChunk: true
+                                }
                             }
-                        }
-                    })
-                    // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+                        })
+                        // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
                     config.optimization.runtimeChunk('single')
                 }
             )
