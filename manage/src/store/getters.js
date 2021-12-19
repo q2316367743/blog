bbe0b15560f22945ac8c888d04446c1e03730a88
浅梦凉城 <m17762618644@163.com>
2021-12-19 21:15:16 +0800

    将后台独立成一个项目

--- manage/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-    sidebar: state => state.app.sidebar,
-    device: state => state.app.device,
-    token: state => state.user.token,
-    avatar: state => state.user.avatar,
-    website: state => state.user.website,
-    name: state => state.user.name
-}
-export default getters
